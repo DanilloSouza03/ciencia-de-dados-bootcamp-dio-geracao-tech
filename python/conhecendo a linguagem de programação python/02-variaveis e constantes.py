9992b2c74c310beb3e9f77488d2755d02e28b7d7
Danillo <danillodesouza03@gmail.com>
2023-05-16 10:49:36 -0300

    Create 02-variaveis e constantes.py

---
+++ python/conhecendo a linguagem de programação python/02-variaveis e constantes.py
@@ -0,0 +1,10 @@
+age = 18
+name = "Guilherme"
+
+age, nome = (20, "Danillo")
+
+print(f'Meu nome é {name}, e eu tenho {age} anos.')
+
+limite_saque_diario = 1000
+
+BRAZILIAN_STATES = ["SP", "RJ", "MG", "SC", "RS"]
