3fa441fe6fb1acc7695f48a8e3fb22ae59a6e907
Danillo <danillodesouza03@gmail.com>
2023-05-16 09:31:32 -0300

    Create 01-tipos de dados.py

---
+++ python/conhecendo a linguagem de programação python/01-tipos de dados.py
@@ -0,0 +1,13 @@
+# Texto (str)
+# Númerico (int, float, complex)
+# Sequência (list, tuple, range) 
+# Mapa (dict)
+# Colecão (set, frozenset)
+# Booleano (bool)
+# Binário (bytes, bytearray, memoryview)
+
+print(1 + 19)
+print(1.5 + 1 + 0.5)
+print(True)
+print(False)
+print("Danillo")
