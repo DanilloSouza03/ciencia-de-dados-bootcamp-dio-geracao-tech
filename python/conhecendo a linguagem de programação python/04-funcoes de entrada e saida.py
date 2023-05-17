a8d756cf9acbe4d9528d68cefb1f397549dce7ff
Danillo <danillodesouza03@gmail.com>
2023-05-17 09:28:56 -0300

    Create 04-funcoes de entrada e saida.py

---
+++ python/conhecendo a linguagem de programação python/04-funcoes de entrada e saida.py
@@ -0,0 +1,9 @@
+
+nome = input("Qual seu nome: ")
+sobrenome = input("Qual seu sobrenome: ")
+
+print(f"Olá {nome} {sobrenome}, Seja bem vindo!")
+print(nome, sobrenome, end="...\n")
+print(nome, sobrenome, sep="#")
+print(nome, sobrenome, sep="#", end="...\n")
+
