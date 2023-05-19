ec888255532da411f644ec72334d7871c620ea45
Danillo <danillodesouza03@gmail.com>
2023-05-18 21:56:33 -0300

    Create 03-operadores de atribuicao.py

---
+++ python/tipos de operadores/03-operadores de atribuicao.py
@@ -0,0 +1,31 @@
+#Atribuição simples
+saldo = 500
+print(saldo)
+
+#Atribuição com adição
+saldo += 200
+print(saldo)
+
+#Atribuição com subtração
+saldo -= 200
+print(saldo)
+
+#Atribuição com multiplicação
+saldo *= 2
+print(saldo)
+
+#Atribuição com divisão
+saldo /= 5
+print(saldo)
+
+#Atribuição com divisão inteira
+saldo //= 5
+print(saldo)
+
+#Atribuição com módulo
+saldo %= 480
+print(saldo)
+
+#Atribuição com exponenciação
+saldo **= 2
+print(saldo)
