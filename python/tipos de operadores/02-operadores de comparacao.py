373d23ac1c603fddd809ace90295352bd0d257bd
Danillo <danillodesouza03@gmail.com>
2023-05-18 21:36:30 -0300

    Create 02-operadores de comparacao.py

---
+++ python/tipos de operadores/02-operadores de comparacao.py
@@ -0,0 +1,21 @@
+
+saldo = 450
+saque = 200
+
+#Igualdade
+print(f"{saldo} é igual a {saque}:", saldo == saque)
+
+#Diferente
+print(f"{saldo} é diferente de {saque}:", saldo != saque)
+
+#Maior
+print(f"{saldo} é maior que {saque}:", saldo > saque)
+
+#Maior ou Igual
+print(f"{saldo} é maior ou igual que {saque}:", saldo >= saque)
+
+#Menor
+print(f"{saldo} é menor que {saque}:", saldo < saque)
+
+#Menor ou Igual
+print(f"{saldo} é menor ou igual que {saque}:", saldo <= saque)
