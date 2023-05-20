c8e9c128fe0f52fc451b6217f881a04b9f9e0a9f
Danillo <danillodesouza03@gmail.com>
2023-05-20 14:08:54 -0300

    Create 03-fatiamento de string.py

---
+++ python/04-manipulacao de strings com python/03-fatiamento de string.py
@@ -0,0 +1,17 @@
+nome_completo = "Danillo Souza Pereira"
+
+print(nome_completo[0])
+
+print(nome_completo[-1])
+
+print(nome_completo[:7])
+
+print(nome_completo[8:])
+
+print(nome_completo[8:13])
+
+print(nome_completo[8:13:2])
+
+print(nome_completo[:])
+
+print(nome_completo[::-1])
