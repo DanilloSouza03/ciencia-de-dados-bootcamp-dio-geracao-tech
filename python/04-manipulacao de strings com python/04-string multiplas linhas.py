ae1110aded7a36d4cf7a059ec1c47aba744d9ba6
Danillo <danillodesouza03@gmail.com>
2023-05-20 22:28:53 -0300

    Create 04-string multiplas linhas.py

---
+++ python/04-manipulacao de strings com python/04-string multiplas linhas.py
@@ -0,0 +1,19 @@
+#String triplas
+
+nome = "Danillo"
+
+mensagem = f"""Olá meu nome é {nome}, Eu estou aprendendo Python"""
+
+print(mensagem)
+
+print(
+    """
+    ==============MENU=================
+        1 - sacar
+        2 - depositar
+        3 - sair
+        
+    Obrigado por utilizar!!
+    ===================================
+    """
+)
