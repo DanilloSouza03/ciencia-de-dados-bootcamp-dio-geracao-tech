e2be0228bbda184fcada82959a217d5c7d89539f
Danillo <danillodesouza03@gmail.com>
2023-05-25 11:05:17 -0300

    Create new lessons

---
+++ python/07-conjuntos/03-iterar conjunto.py
@@ -0,0 +1,7 @@
+america = { "Canadá", "Brasil", "EUA", "México", "Argentina", "Chile"}
+
+for pais in america:
+    print(pais)
+
+for indice, pais in enumerate(america):
+    print(f"{indice}: {pais}")
---
+++ python/07-conjuntos/13-copy.py
@@ -0,0 +1,7 @@
+sorteio = {1, 23}
+
+print(sorteio, id(sorteio))  #{1, 23}
+
+sorteio2 = sorteio.copy()
+
+print(sorteio2, id(sorteio2))  #{1, 23}
